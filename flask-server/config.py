a6cf8ce19e2b20ad3aaedb47bc45906b2b707090
Diego Hernandez <diego.hdz6263@gmail.com>
2023-01-12 22:11:16 -0800

    redis update

--- flask-server/config.py
+++ flask-server/config.py
@@ -15,5 +15,5 @@ class Config:
     SESSION_TYPE = "redis"
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
-    SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
+    SESSION_REDIS = redis.from_url(os.getenv('REDIS_URL'))
     SESSION_REFRESH_EACH_REQUEST = False
